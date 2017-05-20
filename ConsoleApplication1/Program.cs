c38ca9943a0e3c5166794fd671459d52ab520746
Doner <Doner@DONER->
2017-05-20 11:19:07 +0600

    Done

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,23 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            int n = 0;
+            int k = 0;
+            int number = 0;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                int chislo = int.Parse(args[i]);
+                for (int j = 1; j < args.Length; j++)
+                {
+                    int chislo2 = int.Parse(args[j]);
+                    if (chislo == chislo2) { k++;  }
+                }
+                if (k > n) { n = k; number = chislo; }
+                k = 0;
+            }
+            Console.WriteLine("The most frequent number is " + number + " With an occurence of " + n);
+            return;
         }
     }
 }
